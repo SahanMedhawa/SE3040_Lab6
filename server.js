b4886edb8b0d7c61facbffc26287c23352ddacba
Sahan Medhawa <dmsahanmedawa@gmail.com>
2026-03-29 23:13:04 +0530

    basic CRUD implemented

--- server.js
+++ server.js
@@ -10,3 +10,41 @@ app.get('/', (req, res) => {
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+let posts = [];
+let id = 1;
+
+app.post('/posts', (req, res) => {
+    const { title, content } = req.body;
+
+    const newPost = {
+        id: id++,
+        title,
+        content
+    };
+
+    posts.push(newPost);
+    res.json(newPost);
+});
+
+app.get('/posts', (req, res) => {
+    res.json(posts);
+});
+
+app.put('/posts/:id', (req, res) => {
+    const post = posts.find(p => p.id == req.params.id);
+
+    if (!post) {
+        return res.status(404).send('Post not found');
+    }
+
+    post.title = req.body.title;
+    post.content = req.body.content;
+
+    res.json(post);
+});
+
+app.delete('/posts/:id', (req, res) => {
+    posts = posts.filter(p => p.id != req.params.id);
+    res.send('Post deleted');
+});
